77417efc15a598cc8b8520273f78dee3e7344faf
stephen <adeniyistadz@gmail.com>
2021-08-17 17:30:23 +0200

    changes

---
+++ policy-report/trivy-adapter/go.mod
@@ -0,0 +1,20 @@
+module github.com/kubernetes-sigs/wg-policy-prototypes/policy-report/trivy-adapter
+
+go 1.16
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/google/go-containerregistry v0.6.0
+	github.com/google/uuid v1.2.0
+	github.com/spf13/cobra v1.2.1
+	github.com/spf13/pflag v1.0.5
+	k8s.io/api v0.21.3
+	k8s.io/apiextensions-apiserver v0.21.2
+	k8s.io/apimachinery v0.21.3
+	k8s.io/cli-runtime v0.21.3
+	k8s.io/client-go v0.21.3
+	k8s.io/klog v1.0.0
+	k8s.io/utils v0.0.0-20210527160623-6fdb442a123b
+	sigs.k8s.io/controller-runtime v0.9.2
+	sigs.k8s.io/wg-policy-prototypes v0.0.0-20210727080045-c2437d752a22 // indirect
+)
---
+++ policy-report/trivy-adapter/pkg/cmd/root.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"flag"
+	"fmt"
+	"io"
+
+	"github.com/kubernetes-sigs/wg-policy-prototypes/policy-report/trivy-adapter/pkg/imgvuln"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+const (
+	shortMessage = "Trivy adapter security toolkit"
+	longMessage  = `Trivy adapter security toolkit
+imgvuln CLI can be used to find risks, such as vulnerabilities or insecure
+pod descriptors, in Kubernetes workloads. By default, the risk assessment
+reports are stored in the policy report crd.
+$ kubectl create deployment nginx --image nginx:1.16
+Run the vulnerability scanner to generate policy reports:
+$ %[1]s scan policyreports deployment/nginx
+`
+)
+
+func NewRootCmd(buildInfo imgvuln.BuildInfo, args []string, outWriter io.Writer, errWriter io.Writer) *cobra.Command {
+	var cf *genericclioptions.ConfigFlags
+
+	rootCmd := &cobra.Command{
+		Use:           "imgvuln",
+		Short:         shortMessage,
+		Long:          fmt.Sprintf(longMessage, buildInfo.Executable),
+		SilenceErrors: true,
+		SilenceUsage:  true,
+	}
+
+	cf = genericclioptions.NewConfigFlags(true)
+
+	rootCmd.AddCommand(NewInitCmd(buildInfo, cf))
+	rootCmd.AddCommand(NewScanCmd(buildInfo, cf))
+
+	SetGlobalFlags(cf, rootCmd)
+
+	rootCmd.SetArgs(args[1:])
+	rootCmd.SetOut(outWriter)
+	rootCmd.SetErr(errWriter)
+
+	return rootCmd
+}
+
+// Run is the entry point of the imgvuln CLI. It runs the specified
+// command based on the specified args.
+func Run(version imgvuln.BuildInfo, args []string, outWriter io.Writer, errWriter io.Writer) error {
+
+	initFlags()
+
+	return NewRootCmd(version, args, outWriter, errWriter).Execute()
+}
+
+func initFlags() {
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+
+	// Hide all klog flags except for -v
+	flag.CommandLine.VisitAll(func(f *flag.Flag) {
+		if f.Name != "v" {
+			pflag.Lookup(f.Name).Hidden = true
+		}
+	})
+}
